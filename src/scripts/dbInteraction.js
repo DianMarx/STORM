b1f76e268fa27e709e7e6ce03bb00cc3f7d9d23f
SeanThomasHill <u12221458@tuks.co.za>
2015-07-07 13:12:47 +0200

    dbconnection & nodejs

--- src/scripts/dbInteraction.js
+++ src/scripts/dbInteraction.js
@@ -1,4 +1,4 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'), Schema = mongoose.Schema;
 mongoose.connect('mongodb://braSean:qwerty1234@ds053312.mongolab.com:53312/storm');
 
 var db = mongoose.connection;
@@ -6,3 +6,18 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
     console.log("Connection to database was successful.");
 });
+
+//Loose schema
+var mySchema = new Schema({name : String}, {strict:false});
+
+
+loadCollection('testCol');
+
+function collectionToArray(colName)
+{
+    mySchema.set('collection', colName);
+    col = mongoose.model(colName,mySchema);
+    col.find(function (err, docs) {
+        return docs;
+    });
+}
