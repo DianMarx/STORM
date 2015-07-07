be6e053bdeeae8d206989451c5556d687c834bef
SeanThomasHill <u12221458@tuks.co.za>
2015-07-07 14:44:23 +0200

    update dbInteraction

--- src/scripts/dbInteraction.js
+++ src/scripts/dbInteraction.js
@@ -1,23 +1,34 @@
 var mongoose = require('mongoose'), Schema = mongoose.Schema;
 mongoose.connect('mongodb://braSean:qwerty1234@ds053312.mongolab.com:53312/storm');
 
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
     console.log("Connection to database was successful.");
+
 });
+
 
 //Loose schema
 var mySchema = new Schema({name : String}, {strict:false});
 
-
-loadCollection('testCol');
-
 function collectionToArray(colName)
 {
+
+    var data;
     mySchema.set('collection', colName);
     col = mongoose.model(colName,mySchema);
     col.find(function (err, docs) {
-        return docs;
+        data = docs;
+
     });
+    return data;
+
 }
+arr = collectionToArray('testCol');
+console.log(arr);
+
+
+
+
