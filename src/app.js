00ddfe28f3fc228731751af1ad798053905aee58
SeanThomasHill <u12221458@tuks.co.za>
2015-07-16 10:35:03 +0200

    body-parser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 
 var express=require('express');
+var bodyParser = require('body-parser')
+
+
+
 var app=express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 var db = require('./modules/database/dbInteraction');
 require('./router/main')(app);
 app.set('views',__dirname + '/views');
