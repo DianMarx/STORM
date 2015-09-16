65ce19914daab0fd574d34be0cff80add6e5ad07
SeanThomasHill <u12221458@tuks.co.za>
2015-09-16 13:43:15 +0200

    sort by previous groupings

--- src/public/scripts/algorithm.js
+++ src/public/scripts/algorithm.js
@@ -15,18 +15,121 @@ function goShuffle(subs, algs, numGroups)
     {
         switch(algs[i].type)
         {
-            case 'Similar': similarShuffle(subs,numGroups, algs[i].field);
+            case 'Similar': if(algs[i].field == "previousGroups")
+            {
+                similarGroupings(subs,numGroups);
+            }else
+                            similarShuffle(subs,numGroups, algs[i].field);
                             break;
-            case 'Diverse': diverseShuffle(subs,numGroups, algs[i].field);
+            case 'Diverse': if(algs[i].field == "previousGroups")
+            {
+                diverseGroupings(subs,numGroups);
+            }else
+                diverseShuffle(subs,numGroups, algs[i].field);
                 break;
             case 'By Roles': alert("Has yet to be implemented");
                 break;
+
         }
     }
 
 }
+
+function diverseGroupings(subs, numGroups){
+    var arr = sortByPrev(subs);
+
+    var a = 0;
+    for(var p = 0; p < arr.length; p++){
+        subs[arr[p]].group = ++a;
+        if(a == numGroups)
+        {
+            a = 0;
+        }
+    }
+    sendToTables(subs);
+
+}
+
+function similarGroupings(subs, numGroups){
+    var arr = sortByPrev(subs);
+
+    var allowed = getMaxes(subs.length, numGroups);
+    var q = 0, a=0;
+    for(var p = 0; p < arr.length; p++){
+        subs[arr[p]].group = a+1;
+
+        if(q < allowed[a]-1)
+        {
+            q++;
+        }
+        else {a++; q=0;}
+
+    }
+
+
+    sendToTables(subs);
+}
+
+function sortByPrev(subs){
+    //alert("Ja");
+    var arr = [];
+    var N = subs.length;
+    arr.push(0);
+    for(var i = 1; i < N; i++){
+        arr.push(findBest(arr,subs));
+    }
+    //arr is an array of indexes, sorted by cosine similarity
+
+    return arr;
+}
+
+
+function findBest(arr, subs){
+
+    var topIndex = 0;
+    var topCompare = 0;
+    var skip = false;
+    for(var i = 0; i< subs.length; i++){
+        skip = false;
+        for(var p = 0; p < arr.length; p ++)
+        {
+            if(arr[p] == i){
+                skip = true;
+                break;
+            }
+        }
+        if(!skip){
+
+            var sim = similarity(arr, subs[i].previousGroups, subs);
+            if(sim > topCompare){
+                topIndex = i;
+                topCompare = sim;
+            }
+        }
+
+    }
+
+    return topIndex;
+
+}
+function similarity(arr,b, subs){
+    var c = 0;
+    //alert(b);
+    for(var i = 0; i < arr.length; i++){
+
+        var temp = subs[arr[i]].previousGroups;
+
+        for(var p = 0; p < b.length; p++){
+            //alert(temp[p] + " = " + b[p]);
+            if(temp[p] == b[p]){ c++;}
+
+        }
+    }
+    return c;
+}
 function diverseShuffle(subs,numGroups,field)
 {
+
     function compare(a,b) {
         if (a[field] < b[field])
             return -1;
@@ -78,9 +181,11 @@ function similarShuffle(subs,numGroups,field)
 //loads sorted to teams
 function sendToTables(subs)
 {
+
 var numSubs = subs.length;
     for(var i = 0; i < numSubs; i++)
     {
+
         var to = $('.' + subs[i].group).find('.subjBody');
 
         $('#' + subs[i].id).appendTo(to);
