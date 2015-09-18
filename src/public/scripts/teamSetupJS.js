a65026164e938ea94e7e54b13b7cf45c68d17414
Johann Marx <u12105202@tuks.co.za>
2015-09-18 22:30:58 +0200

    Testing first table design

--- src/public/scripts/teamSetupJS.js
+++ src/public/scripts/teamSetupJS.js
@@ -889,3 +889,29 @@ function SortBy(arr,key)
     });
 }
 
+//Function to Draw the Chart
+
+function drawChart() {
+
+    //Create the data table
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+        ['Mushrooms', 3],
+        ['Onions', 1],
+        ['Olives', 1],
+        ['Zucchini', 1],
+        ['Pepperoni', 2]
+    ]);
+
+    // Set chart options
+    var options = {'title':'How Much Pizza I Ate Last Night',
+        'width':800,
+        'height':600};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chartDiv'));
+    chart.draw(data, options);
+
+}
