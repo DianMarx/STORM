f10541ff0a5d75403477be0a7b72dc609c80dc29
Johann Marx <u12105202@tuks.co.za>
2015-09-22 07:28:26 +0200

    Implementing Charts into develop

--- src/public/scripts/teamSetupJS.js
+++ src/public/scripts/teamSetupJS.js
@@ -64,6 +64,10 @@ $(document).ready(function(e) {
         field = field.replace(' ','_');
         $("th#"+ field).remove();
         $("td#"+ field).remove();
+        var elem = document.getElementById("poolChart");
+        elem.parentElement.removeChild(elem);
+
+        $('#mainChart').append("<div id='poolChart'></div>")
     }
 
         var div = $("<form id='selection'>Select variable to shuffle by:<br></form><br>").insertAfter("#shuffleHeading");
@@ -960,4 +964,56 @@ function addField(field)
         var sub = subjects[i];
         $("#"+sub.id+"group").closest('tr').append("<td id='"+field +"'>"+sub[field]+"</td>");
     }
+
+    //Adding Charts for each Team
+    for(var k = 1;k < numTeamGroups;k++)
+    {
+        $('#poolChart').append("<div class='charts "+(k)+"'>Team "+(k)+"<div id='chartDiv "+(k)+"'</div> </div>");
+        drawChart(k,field);
+    }
+}
+
+
+//Function to Draw the Chart
+function drawChart(n,tField) {
+    var columnCount = 0;
+
+    var data = new google.visualization.DataTable();
+    var dataArray = [];
+    data.addColumn('number', 'values');
+    data.addColumn('number', 'values');
+    dataArray.push(1);
+    dataArray.push(100);
+    for(var t=0;t<subjects.length;t++)
+    {
+        if(subjects[t].group == n)
+        {
+            columnCount++;
+            data.addColumn({id:'i'+t, type:'number', role:'interval'});
+        }
+    }
+//alert(columnCount);
+    for(var q = 0;q < subjects.length;q++)
+    {
+        if(subjects[q].group == n)
+        {
+            var subje = subjects[q];
+            var tempValue = subje[tField];
+            dataArray.push(tempValue);
+        }
+    }
+    //alert(dataArray);
+    data.addRow(dataArray);
+    //alert('test');
+
+    var chartOptions = {
+        title:'Points, default',
+        curveType:'function',
+        lineWidth: 2,
+        series: [{'color': '#D3362D'}],
+        intervals: { 'style':'points', pointSize: 2 },
+        legend: 'none',
+    };
+    var chart = new google.visualization.LineChart(document.getElementById('chartDiv '+n));
+    chart.draw(data, chartOptions);
 }
