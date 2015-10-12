8e1840cfc2adf7d56c2fe8c99fb8c3bb23a51cc5
Johann Marx <u12105202@tuks.co.za>
2015-10-12 14:46:49 +0200

    Integrated working Charts File

--- src/public/scripts/teamSetupJS.js
+++ src/public/scripts/teamSetupJS.js
@@ -1019,6 +1019,19 @@ function populateSubjectPool()
         drop: function(event, ui) {
             //var temp = alert();
             $(ui.draggable).detach().css({top: 0,left: 0}).appendTo($(this).find('.subjBody'));
+            var str = $(ui.draggable).attr('id');
+            alert(str);
+            var res = str.replace("group", "");
+            alert(res);
+            for( var su = 0; su < subjects.length;su++)
+            {
+                if(subjects[su].id == res) {
+                    subjects[su].group = $(ui.draggable).parent().attr('id')
+                    alert(subjects[su].group);
+                }
+
+
+            }
             GroupsChanged();
         }
     });
@@ -1051,102 +1064,21 @@ function addField(field)
     //Adding Charts for each Team
     for(var k = 1;k < numTeamGroups;k++)
     {
-        $('#poolChart').append("<div class='charts "+(k)+"'>Team "+(k)+"<div id='chartDiv "+(k)+"'</div> </div>");
         drawChart(k,field);
     }
+    var globalDiv = document.getElementById('globalChart');
+    var gAve = getGlobalAverage(field);
+    var gstdDev = globalstdDev(gAve,field);
+    globalDiv.innerHTML = 'Global Average = ' + gAve;
+    globalDiv.innerHTML = globalDiv.innerHTML + '<br> Global Standard Deviation = ' + gstdDev;
 }
-
-
-//Function to Draw the Chart
-function drawChart(n,tField) {
-    var columnCount = 0;
-
-    var data = new google.visualization.DataTable();
-    var dataArray = [];
-    data.addColumn('number', 'values');
-    data.addColumn('number', 'values');
-    dataArray.push(1);
-    dataArray.push(100);
-    for(var t=0;t<subjects.length;t++)
-    {
-        if(subjects[t].group == n)
-        {
-            columnCount++;
-            data.addColumn({id:'i'+t, type:'number', role:'interval'});
-        }
-    }
-//alert(columnCount);
-    for(var q = 0;q < subjects.length;q++)
-    {
-        if(subjects[q].group == n)
-        {
-            var subje = subjects[q];
-            var tempValue = subje[tField];
-            dataArray.push(tempValue);
-        }
-    }
-    //alert(dataArray);
-    data.addRow(dataArray);
-    //alert('test');
-
-    var chartOptions = {
-        title:'Points, default',
-        curveType:'function',
-        lineWidth: 2,
-        series: [{'color': '#D3362D'}],
-        intervals: { 'style':'points', pointSize: 2 },
-        legend: 'none'
-    };
-    var chart = new google.visualization.LineChart(document.getElementById('chartDiv '+n));
-    chart.draw(data, chartOptions);
-}
-
-/*
-function drawExampleChart() {
-    var Edata = new google.visualization.DataTable();
-    var columnCount;
-    Edata.addColumn('number', 'values');
-    Edata.addColumn('number', 'values');
-        Edata.addColumn({id:'i'+t, type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-    Edata.addColumn({id:'i', type:'number', role:'interval'});
-
-    Edata.addRows([[1,10,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
-        [2,21,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40],
-        [3,32,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60]]);
-}
-
-var EchartOptions = {
-    title:'Points, default',
-    curveType:'function',
-    lineWidth: 2,
-    series: [{'color': '#D3362D'}],
-    intervals: { 'style':'points', pointSize: 2 },
-    legend: 'none';
-};
-var chart = new google.visualization.LineChart(document.getElementById('exampleChart'));
-chart.draw(Edata, EchartOptions);
-}*/
 
 function GroupsChanged()
 {
     var numGroups = numTeamGroups -1;
     numManipulations += 1;
+    for(var k = 1;k < numTeamGroups;k++)
+    {
+        updateChart(k,'Mark');
+    }
 }
